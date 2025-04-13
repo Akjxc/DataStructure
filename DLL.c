bb2c247b95b0161c9adb02606d5e2c1fc77ea85e
Akjxc <60183793+Akjxc@users.noreply.github.com>
2025-04-13 07:34:44 +0000

    더블 링크드 리스트 완료

--- DLL.c
+++ DLL.c
@@ -29,7 +29,7 @@ void DLL_AppendNode(Node** Head, Node* NewNode){
     }
     else {
         Node* Tail = *Head;
-        while(Tail->NextNode != NULL);{
+        while(Tail->NextNode != NULL){
             Tail = Tail->NextNode;
         }
         Tail->NextNode = NewNode;
@@ -50,15 +50,91 @@ Node* DLL_GetNodeAt(Node* Head, int Location){
 void DLL_RemoveNode(Node** Head, Node* Remove){
     if(*Head == Remove){
         *Head = Remove->NextNode;
+        if((*Head) != NULL)
+            (*Head)->PrevNode = NULL;
+
+        Remove->PrevNode = NULL;
+        Remove->NextNode = NULL;
     }
     else {
-        (Remove->PrevNode)->NextNode = Remove->NextNode;
-        (Remove->NextNode)->PrevNode = Remove->PrevNode;
+        Node* Temp = Remove;
+
+        if(Remove->PrevNode != NULL)
+            Remove->PrevNode->NextNode = Temp->NextNode;
+
+        if(Remove->NextNode != NULL)
+            Remove->NextNode->PrevNode = Temp->PrevNode;
+
         Remove->NextNode = NULL;
         Remove->PrevNode = NULL;
     }
 }
 
-int main(void){
+void DLL_InsertAfter(Node* Current, Node* NewNode){
+    NewNode->NextNode = Current->NextNode;
+    NewNode->PrevNode = Current;
 
+    if(Current != NULL){
+        Current->NextNode = NewNode;
+        Current->NextNode->PrevNode = NewNode;
+    }
+}
+
+int DLL_GetNodeCount(Node* Head){
+    unsigned int Count = 0; // unsigned - 음수 표현 불가, 0과 양수만
+    Node* Current = Head;
+
+    while(Current != NULL){
+        Current = Current->NextNode;
+        Count++;
+    }
+
+    return Count;
+}
+
+void PrintNode(Node* _Node){
+    if(_Node->PrevNode == NULL)
+        printf("Prev : NULL\t");
+    else 
+        printf("Prev : %d\t", _Node->PrevNode->Data);
+    
+    printf("Current : %d\t", _Node->Data);
+
+    if(_Node->NextNode == NULL)
+        printf("Next : NULL\n");
+    else
+        printf("Next : %d\n", _Node->NextNode->Data);
+}
+
+void PrintReverse(Node* Head){
+    unsigned int Count = 0;
+    Node* Tail = Head;
+    while(Tail->NextNode != NULL){
+        Tail = Tail->NextNode;
+        Count++;
+    }
+
+    while(Tail != NULL){
+        printf("List[%d] : %d\n", Count, Tail->Data);
+        Tail = Tail->PrevNode;
+        Count--;
+    }
+}
+
+int main(void){
+    Node* List = NULL;
+    Node* NewNode = NULL;
+
+    for(int i = 0; i < 5; i++){
+        NewNode = DLL_CreateNode(i);
+        DLL_AppendNode(&List, NewNode);
+    }
+
+    for(int i = 0; i < DLL_GetNodeCount(List); i++){
+        PrintNode(DLL_GetNodeAt(List, i));
+    }
+
+    printf("-------------------------------------------\n");
+
+    PrintReverse(List);
 }
